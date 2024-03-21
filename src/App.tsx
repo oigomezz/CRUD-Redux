b8047c6dbf3757fa4a399b62f0951be253928886
Oscar Ivan Gomez Zamudio <oigomezz@unal.edu.co>
2024-03-21 12:12:10 -0500

    Se agraga las funcionalidades de Crear y Eliminar usuarios

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import { ListOfUsers } from "./components/ListOfUsers";
+import { CreateNewUser } from "./components/CreateNewUser";
+import { Toaster } from "sonner";
 
 function App() {
-  return <h1>Mi proyecto con Redux</h1>;
+  return (
+    <>
+      <ListOfUsers />
+      <CreateNewUser />
+      <Toaster richColors />
+    </>
+  );
 }
 
 export default App;
